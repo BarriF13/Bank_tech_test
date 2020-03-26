69ce3d5378cfc9fcf81091f11a3f4a1a79c9c701
Barri <info@bdesignstudio.co.uk>
2020-03-26 10:40:37 +0000

    Customer can add deposite

--- lib/deposite.rb
+++ lib/deposite.rb
@@ -1,3 +1,14 @@
 class Deposite
 
+  attr_reader :credit
+
+  def initialize(credit)
+    @credit = credit
+  end
+
+  def add
+    credit.map do |type, amount| 
+      "%s £%.2f" % [type, amount]
+    end.join(", ")
+  end
 end
--- spec/deposite_spec.rb
+++ spec/deposite_spec.rb
@@ -1,5 +1,19 @@
 require 'deposite'
 
 describe Deposite do
+  subject(:deposite) { described_class.new(credit) }
+  let (:credit) do
+    {
+      credit: 2000
+    }
+  end
+
+  it 'can add credit to the account' do
+    expect(deposite.credit).to eq(credit)
+  end
   
+  it ": adds credit to the account" do
+    added_deposite = "credit £2000.00"
+    expect(deposite.add).to eq(added_deposite)
+  end
 end
