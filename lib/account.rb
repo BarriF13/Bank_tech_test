f3f9e767ded19a1c3d1f1227cd1a85f671d8b999
Barri <info@bdesignstudio.co.uk>
2020-03-26 19:15:08 +0000

    Set up

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,6 @@
+
 class Account
 
-  def initialize(deposite: )
-    @deposite = deposite
-    # @withdraw = withdraw
-  end
-
-  def add_deposite
-    deposite.add
-  end
   
-  # def draw_debit
-  #   withdraw.draw
-  # end
-
-  private 
-
-  attr_reader :deposite
-  # :withdraw
 
 end
