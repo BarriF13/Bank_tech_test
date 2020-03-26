058bc5516d642726ea408f3ab0145e2963cf19d9
Barri <info@bdesignstudio.co.uk>
2020-03-26 08:12:30 +0000

    Set up the first test to fail

--- lib/account.rb
+++
@@ -1,9 +0,0 @@
-class Account
-    def initilaize (debit)
-      @debit = debit
-    end
-
-    def deposite
-    end
-end
-
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,5 @@
 require './lib/account'
 
 describe Account do
-  it 'deposite to an account' do
-    expect(subject).to respond_to :deposite
-  end
+
 end
